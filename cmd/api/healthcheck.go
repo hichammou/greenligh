cf12e5327994dbd91d38731a0c4c3bacc477a074
hicham_mou <moulilihicham8@gmail.com>
2024-12-03 17:39:20 +0100

    add app dependecies and a route for healthcheck, and basic routes for movies

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encoutred a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module greenlight.hichammou
+
+go 1.23.3
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
