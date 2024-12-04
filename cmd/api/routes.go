855619aa30201a80cd532d1d9b39428f2f038016
hicham_mou <moulilihicham8@gmail.com>
2024-12-04 17:19:21 +0100

    set httprouter's not found and method not allowed to custom json errors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,13 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFoundResponse(w, r)
+	})
+	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.methodNotAllowedResponse(w, r)
+	})
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheck)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.ShowMovieHandler)
