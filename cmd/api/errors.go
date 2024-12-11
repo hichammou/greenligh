7128efc1c68e86514e5ca9703b8c80d5f35b711c
hicham_mou <moulilihicham8@gmail.com>
2024-12-11 16:25:46 +0100

    add the custom logger to the app

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -6,7 +6,10 @@ import (
 )
 
 func (app *application) logError(r *http.Request, err error) {
-	app.logger.Print(err)
+	app.logger.PrintError(err, map[string]string{
+		"request_method": r.Method,
+		"request_url":    r.URL.String(),
+	})
 }
 
 func (app *application) faildValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
