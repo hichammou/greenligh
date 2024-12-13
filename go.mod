360572558c262f466a9efa1fa3218269054ecf36
hicham_mou <moulilihicham8@gmail.com>
2024-12-13 09:17:34 +0100

    create User struct and validation rules for its fields

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module greenlight.hichammou
 go 1.23.3
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/lib/pq v1.10.2 // indirect
-	golang.org/x/time v0.8.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.2
+	golang.org/x/crypto v0.31.0
+	golang.org/x/time v0.8.0
 )
---
+++ internal/data/users.go
@@ -0,0 +1,57 @@
+package data
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+	"greenlight.hichammou/internal/validator"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+// Set Generates a hashed password for the user
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+// Matches checks if the plaintextPassword matches the user's hashed password
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
+// Validation rules
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Che
+}
