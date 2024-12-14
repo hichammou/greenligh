a77b6cca5b1e4dd0d2f92f1fd6d51c63f6483b25
hichammou <moulilihicham8@gmail.com>
2024-12-14 18:18:53 +0100

    create Token struct and a token generator function for user validation

---
+++ internal/data/tokens.go
@@ -0,0 +1,25 @@
+package data
+
+import "time"
+
+// Define a constant for the token scope.
+const (
+	ScopeActivation = "activation"
+)
+
+type Token struct {
+	Plaintext string
+	Hash      []byte
+	UserID    int64
+	Expiry    time.Time
+	Scope     string
+}
+
+func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserID: userID,
+		Expiry: time.Now().Add(ttl), // (time-to-live)
+	}
+
+	return token, nil
+}
