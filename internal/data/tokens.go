1ea3606fa3eff26987336784c35882c7371ac5cd
hichammou <moulilihicham8@gmail.com>
2024-12-14 18:43:51 +0100

    add TokenModel and Insert and DeleteAllForUser methods and add the TokenModel as dependency to the application

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,6 +1,13 @@
 package data
 
-import "time"
+import (
+	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"database/sql"
+	"encoding/base32"
+	"time"
+)
 
 // Define a constant for the token scope.
 const (
@@ -15,11 +22,65 @@ type Token struct {
 	Scope     string
 }
 
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token, err := generateToken(userID, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Insert(token)
+
+	return token, err
+}
+
+func (m TokenModel) Insert(token *Token) error {
+	query := `INSERT INTO tokens (hash, user_id, expiry, scope)
+            VALUES ($1, $2, $3, $4)`
+
+	args := []interface{}{token.Hash, token.UserID, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, args...)
+
+	return err
+}
+
+func (m TokenModel) DeleteAllForUser(scope string, userId int64) error {
+	query := `DELETE FROM tokens WHERE user_id = $1 AND scope = $2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, scope, userId)
+	return err
+}
+
 func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
 	token := &Token{
 		UserID: userID,
 		Expiry: time.Now().Add(ttl), // (time-to-live)
+		Scope:  scope,
 	}
+
+	// Initialize a zero-valued byte slice with a length of 16 bytes.
+	randomeBytes := make([]byte, 16)
+
+	// Use the Read() function to fill the byte slice with random bytes from ur operating sys's CSPRNG
+	_, err := rand.Read(randomeBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomeBytes)
+
+	hash := sha256.Sum256([]byte(token.Plaintext))
+	token.Hash = hash[:]
 
 	return token, nil
 }
