67d7440bff404b4b1c850dcbc26617b3129c963e
hichammou <moulilihicham8@gmail.com>
2024-12-15 12:18:23 +0100

    update registerHandler to send the activate Token with the welcome email

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"greenlight.hichammou/internal/data"
 	"greenlight.hichammou/internal/validator"
@@ -52,11 +53,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	app.logger.PrintInfo("email", map[string]string{"email": user.Email})
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
 	// Use the background helper to execute an anonymous function that sends the welcome email
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 			return
