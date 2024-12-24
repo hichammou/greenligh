7aeee687509e6d43d089e75cc7f084796ffc94ae
hichammou <moulilihicham8@gmail.com>
2024-12-24 17:43:35 +0100

    add route to account activation token

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -33,6 +33,7 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/password-reset", app.createPasswordResetTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
 
 	router.Handler(http.MethodGet, "/debug/var", expvar.Handler())
 
